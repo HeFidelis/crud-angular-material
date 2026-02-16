7b1200653f0107d9dab46b091127457fcf1617ec
HeFidelis <heitorfidelis81@gmail.com>
2026-02-16 18:29:30 -0400

    Finalizando a parte visual do formulário

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -4,6 +4,8 @@ import { MatCardModule } from '@angular/material/card'
 import { FormsModule } from '@angular/forms'
 import { MatFormField, MatLabel } from '@angular/material/form-field'
 import { MatInputModule} from '@angular/material/input'
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-cadastro',
@@ -12,8 +14,10 @@ import { MatInputModule} from '@angular/material/input'
     MatCardModule,
     FormsModule, MatFormField,
     MatLabel,
-    MatInputModule
-  ],
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
