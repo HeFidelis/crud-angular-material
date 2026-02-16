ca3476f82160bf668aba68ccbd6603bc48b69622
HeFidelis <heitorfidelis81@gmail.com>
2026-02-16 19:04:31 -0400

    Trabalhando com Services

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -7,6 +7,7 @@ import { MatInputModule} from '@angular/material/input'
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { Cliente } from './cliente';
+import { ClienteService } from '../cliente.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -26,7 +27,11 @@ export class Cadastro {
 
   cliente: Cliente = Cliente.newCliente();
 
+  constructor(private service: ClienteService){
+
+  }
+
   salvar(){
-    console.log("Dados Cliente: ", this.cliente);
+    this.service.salvar(this.cliente);
   }
 }
