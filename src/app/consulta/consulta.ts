8209077b2c46591c86078ce4101dbcb0c6812923
HeFidelis <heitorfidelis81@gmail.com>
2026-02-17 15:13:25 -0400

    Navegando entre páginas e passando parâmetros

--- src/app/consulta/consulta.ts
+++ src/app/consulta/consulta.ts
@@ -9,6 +9,7 @@ import { MatTableModule} from '@angular/material/table'
 import { MatButtonModule } from '@angular/material/button';
 import { ClienteService } from '../cliente.service';
 import { Cliente } from '../cadastro/cliente';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-consulta',
@@ -29,9 +30,12 @@ export class Consulta implements OnInit {
 
   nomeBusca: string = '';
   listaClientes: Cliente[] = [];
-  colunasTable: string[] = ["id", "nome", "cpf", "dataNascimento", "email"];
+  colunasTable: string[] = ["id", "nome", "cpf", "dataNascimento", "email", "acoes"];
 
-  constructor(private service: ClienteService){
+  constructor(
+    private service: ClienteService,
+    private router: Router
+  ){
 
   }
 
@@ -41,6 +45,10 @@ export class Consulta implements OnInit {
 
   pesquisar(){
     this.listaClientes = this.service.pesquisarClientes(this.nomeBusca);
+  }
+
+  preparaEditar(id: string){
+    this.router.navigate(['/cadastro'], { queryParams: {"id": id} } );
   }
 
 }
