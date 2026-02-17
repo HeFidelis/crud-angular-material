49ecb8e996224931a6166e53ec9723ffb59923b7
HeFidelis <heitorfidelis81@gmail.com>
2026-02-17 16:06:24 -0400

    Deletando registros

--- src/app/consulta/consulta.ts
+++ src/app/consulta/consulta.ts
@@ -31,6 +31,7 @@ export class Consulta implements OnInit {
   nomeBusca: string = '';
   listaClientes: Cliente[] = [];
   colunasTable: string[] = ["id", "nome", "cpf", "dataNascimento", "email", "acoes"];
+  deletando: boolean = false;
 
   constructor(
     private service: ClienteService,
@@ -49,6 +50,16 @@ export class Consulta implements OnInit {
 
   preparaEditar(id: string){
     this.router.navigate(['/cadastro'], { queryParams: {"id": id} } );
+  }
+
+  preparaDeletar(){
+    this.deletando = true;
+  }
+
+  deletar(cliente: Cliente){
+    this.service.deletar(cliente);
+    this.listaClientes = this.service.pesquisarClientes('');
+    this.deletando = false;
   }
 
 }
