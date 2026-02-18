29b80b5d33b4357c329e0aa034f4338c84ab4673
HeFidelis <heitorfidelis81@gmail.com>
2026-02-17 22:33:33 -0400

    Consumindo a API de listagem de UF'S

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -11,6 +11,8 @@ import { Cliente } from './cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask'
+import { Brasilapi } from '../brasilapi.service';
+import { Estado, Municipio } from '../brasilapi.models';
 
 @Component({
   selector: 'app-cadastro',
@@ -34,14 +36,15 @@ export class Cadastro implements OnInit {
   cliente: Cliente = Cliente.newCliente();
   atualizando: boolean = false;
   snack: MatSnackBar = inject(MatSnackBar);
+  estados: Estado[] = [];
+  municipios: Municipio[] = [];
 
   constructor(
     private service: ClienteService,
+    private brasilApiService: Brasilapi,
     private route: ActivatedRoute,
     private router: Router
-  ){
-
-  }
+  ){}
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe( (query: any) => {
@@ -54,6 +57,16 @@ export class Cadastro implements OnInit {
           this.cliente = clienteEncontrado;
         }
       }
+    })
+
+    this.carregarUFs();
+  }
+
+  carregarUFs(){
+    // observable subscriber
+    this.brasilApiService.listarUFs().subscribe({
+      next: listaEstados => this.estados = listaEstados,
+      error: erro => console.log("ocorreu um erro: ", erro)
     })
   }
 
